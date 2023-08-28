5c56010c68852e6cfae2b7b1485821c452185432
Brandon <nardnob@users.noreply.github.com>
2023-08-28 15:57:47 -0500

    Utilize children in EmailToast component

--- src/modals/ContactModal.js
+++ src/modals/ContactModal.js
@@ -15,8 +15,8 @@ export default function ContactModal({ show, onClose }) {
     function handleSend() {
         setIsLoading(true);
         setTimeout(() => {
-            setIsLoading(false);
             handleClose();
+            setIsLoading(false);
             setShowEmailToast(true);
         }, 2500);
     }
@@ -27,32 +27,30 @@ export default function ContactModal({ show, onClose }) {
                 <Modal.Header closeButton>
                     <Modal.Title>Contact Me</Modal.Title>
                 </Modal.Header>
-                {isLoading && 
                     <Modal.Body>
+                    {isLoading && 
                         <Loading />
-                    </Modal.Body>
-                }
-                {!isLoading &&
-                    <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="contactForm.email">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control 
-                                type="email" 
-                                placeholder="Enter your email address" 
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="contactForm.message">
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control 
-                                as="textarea" 
-                                rows={3} 
-                                placeholder="Enter your message"
-                            />
-                        </Form.Group>
-                    </Form>
-                    </Modal.Body>
-                }
+                    }
+                    {!isLoading &&
+                        <Form>
+                            <Form.Group className="mb-3" controlId="contactForm.email">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control 
+                                    type="email" 
+                                    placeholder="Enter your email address" 
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="contactForm.message">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control 
+                                    as="textarea" 
+                                    rows={3} 
+                                    placeholder="Enter your message"
+                                />
+                            </Form.Group>
+                        </Form>
+                    }
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
@@ -65,7 +63,9 @@ export default function ContactModal({ show, onClose }) {
             <EmailToast 
                 show={showEmailToast}
                 onClose={() => setShowEmailToast(false) }
-            />
+            >
+                Thanks for the message! I will respond as soon as possible.
+            </EmailToast>
         </>
     );
 }
--- src/toasts/EmailToast.js
+++ src/toasts/EmailToast.js
@@ -1,14 +1,14 @@
 import Toast from 'react-bootstrap/Toast';
 import './EmailToast.css';
 
-export default function EmailToast({ show, onClose}) {
+export default function EmailToast({ show, onClose, children }) {
     return (
         <Toast show={show} onClose={() => onClose()} className="email-toast">
             <Toast.Header>
                 <strong className="me-auto">Brandon Dixson</strong>
                 <small>just now</small>
             </Toast.Header>
-            <Toast.Body>Thanks for the message! I will respond as soon as possible.</Toast.Body>
+            <Toast.Body>{children}</Toast.Body>
         </Toast>
     );
 }
