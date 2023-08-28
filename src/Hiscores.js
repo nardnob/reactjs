e6121ecd69f2352644fc578693cf16dcbde37a1a
Brandon <nardnob@users.noreply.github.com>
2023-08-28 01:32:24 -0500

    Display mocked-up hiscores results

--- src/Hiscores.js
+++ src/Hiscores.js
@@ -4,6 +4,8 @@ import './Hiscores.css';
 
 export default function Hiscores() {
     const [isLoading, setIsLoading] = useState(false);
+    const [username, setUsername] = useState('');
+    const [hiscores, setHiscores] = useState(null);
 
     if (isLoading) {
         return (
@@ -16,22 +18,45 @@ export default function Hiscores() {
         return (
             <div className="hiscores">
                 <div className="hiscores-title">Hiscores</div>
-                <input 
-                    type="text"
-                    placeholder="Enter a username"
-                    className="hiscores-input"
-                />
-                <button 
-                    className="hiscores-submit"
-                    onClick={handleSubmit}
-                >
-                    Submit
-                </button>
+                <div>
+                    <input 
+                        type="text"
+                        placeholder="Enter a username"
+                        className="hiscores-input"
+                        onChange={e => setUsername(e.target.value)}
+                        value={username}
+                    />
+                    <button 
+                        className="hiscores-submit"
+                        onClick={handleSubmit}
+                    >
+                        Submit
+                    </button>
+                </div>
+                {hiscores && 
+                <div className="hiscores-skills">
+                    <div>Hiscores for {hiscores.username}</div>
+                    <div>Attack: {hiscores.skills.attack}</div>
+                    <div>Strength: {hiscores.skills.strength}</div>
+                    <div>Defence: {hiscores.skills.defence}</div>
+                </div>
+                }
             </div>
         );
     }
 
     function handleSubmit() {
         setIsLoading(true);
+        setTimeout(() => {
+            setHiscores({
+                username: username,
+                skills: {
+                    attack: 97,
+                    strength: 99,
+                    defence: 90
+                }
+            })
+            setIsLoading(false);
+        }, 3000);
     }
 }
