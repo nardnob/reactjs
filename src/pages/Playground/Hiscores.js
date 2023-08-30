2dbafab32d5a6b9812fd5fec212df6fd1c6d42e3
Brandon <nardnob@users.noreply.github.com>
2023-08-29 21:12:45 -0500

    Update the Hiscores component to utilize an InputGroup

--- src/pages/Playground/Hiscores.js
+++ src/pages/Playground/Hiscores.js
@@ -1,50 +1,14 @@
 import { useState } from 'react';
 import Loading from '../../common/utilities/Loading.js';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import './Hiscores.css';
 
 export default function Hiscores() {
     const [isLoading, setIsLoading] = useState(false);
     const [username, setUsername] = useState('');
     const [hiscores, setHiscores] = useState(null);
-
-    if (isLoading) {
-        return (
-            <div className="hiscores">
-                <div className="hiscores-title">Hiscores</div>
-                <Loading />
-            </div>
-        );
-    } else {
-        return (
-            <div className="hiscores">
-                <div className="hiscores-title">Hiscores</div>
-                <div>
-                    <input 
-                        type="text"
-                        placeholder="Enter a username"
-                        className="hiscores-input"
-                        onChange={e => setUsername(e.target.value)}
-                        value={username}
-                    />
-                    <Button 
-                        className="hiscores-submit"
-                        onClick={handleSubmit}
-                    >
-                        Submit
-                    </Button>
-                </div>
-                {hiscores && 
-                <div className="hiscores-skills">
-                    <div>Hiscores for {hiscores.username}</div>
-                    <div>Attack: {hiscores.skills.attack}</div>
-                    <div>Strength: {hiscores.skills.strength}</div>
-                    <div>Defence: {hiscores.skills.defence}</div>
-                </div>
-                }
-            </div>
-        );
-    }
 
     function handleSubmit() {
         setIsLoading(true);
@@ -59,5 +23,43 @@ export default function Hiscores() {
             })
             setIsLoading(false);
         }, 3000);
+    }
+
+    if (isLoading) {
+        return (
+            <div className="hiscores">
+                <div className="hiscores-title">Hiscores</div>
+                <Loading />
+            </div>
+        );
+    } else {
+        return (
+            <div className="hiscores">
+                <div className="hiscores-title">Hiscores</div>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        placeholder="Username"
+                        aria-label="Username"
+                        className="hiscores-input"
+                        onChange={e => setUsername(e.target.value)}
+                        value={username}
+                    />
+                    <Button 
+                        variant="primary" 
+                        className="hiscores-submit"
+                        onClick={handleSubmit}>
+                        Lookup
+                    </Button>
+                </InputGroup>
+                {hiscores && 
+                <div className="hiscores-skills">
+                    <div>Hiscores for {hiscores.username}</div>
+                    <div>Attack: {hiscores.skills.attack}</div>
+                    <div>Strength: {hiscores.skills.strength}</div>
+                    <div>Defence: {hiscores.skills.defence}</div>
+                </div>
+                }
+            </div>
+        );
     }
 }
