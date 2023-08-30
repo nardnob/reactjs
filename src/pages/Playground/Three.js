f1f307092a36252616dc068d4dc06baa744bcaa7
Brandon <nardnob@users.noreply.github.com>
2023-08-30 16:22:52 -0500

    Add "HI" in the Three component.

--- src/pages/Playground/Three.js
+++ src/pages/Playground/Three.js
@@ -11,7 +11,8 @@ function Box(props) {
     const [hovered, setHover] = useState(false)
     const [clicked, setClick] = useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => (ref.current.rotation.x += delta))
+    //useFrame((state, delta) => (ref.current.rotation.x += delta))
+
     // Return the view, these are regular Threejs elements expressed in JSX
     return (
         <mesh
@@ -21,8 +22,8 @@ function Box(props) {
             onClick={(event) => setClick(!clicked)}
             onPointerOver={(event) => (event.stopPropagation(), setHover(true))}
             onPointerOut={(event) => setHover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <boxGeometry args={[props.boxWidth, props.boxHeight, 1]} />
+            <meshStandardMaterial color={hovered ? 'gray' : 'royalblue'} />
         </mesh>
     )
 }
@@ -33,10 +34,17 @@ export default function Three() {
                 <div className="three-canvas-container">
                     <Canvas className="three-canvas">
                         <ambientLight intensity={0.5} />
-                        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                        <pointLight position={[-10, -10, -10]} />
-                        <Box position={[-1.2, 0, 0]} />
-                        <Box position={[1.2, 0, 0]} />
+                        <spotLight intensity={700} position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                        <spotLight intensity={700} position={[30, 10, 10]} angle={0.15} penumbra={1} />
+                        <pointLight intensity={700} position={[-10, -10, -10]} />
+                        <pointLight intensity={700} position={[10, 10, 10]} />
+                        <Box position={[-3, 0, 0]} boxWidth={1} boxHeight={3} />
+                        <Box position={[-2, 0, 0]} boxWidth={1} boxHeight={1} />
+                        <Box position={[-1, 0, 0]} boxWidth={1} boxHeight={3} />
+
+                        <Box position={[2, 1, 0]} boxWidth={3} boxHeight={1} />
+                        <Box position={[2, 0, 0]} boxWidth={1} boxHeight={1} />
+                        <Box position={[2, -1, 0]} boxWidth={3} boxHeight={1} />
                         <OrbitControls />
                     </Canvas>
                 </div>
