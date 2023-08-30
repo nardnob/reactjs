1a223d4115b432d994b2f5106fbdc9c3949bc5d0
Brandon <nardnob@users.noreply.github.com>
2023-08-30 02:08:22 -0500

    More development (#3)
    * Update the Hiscores component to utilize an InputGroup
    
    * Adjust styling of NavigationBar.
    Give body a background color.
    
    * Set the default font.
    Set overview-header font.
    
    * Minor styling tweaks.
    
    * Refactor out SkillBadges component from Skills
    
    * Work on Hiscores component
    
    * Remove unnecessary import

--- src/pages/About/Education.js
+++ src/pages/About/Education.js
@@ -1,4 +1,5 @@
 import Card from 'react-bootstrap/Card';
+import './Education.css';
 
 export default function Education() {
     return (
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,13 @@
 import Overview from './Overview';
-import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 
 export default function Home() {
     return (
         <>
-            <Card className="app">
-                <Card.Body>
-                    <Overview />
-                </Card.Body>
-            </Card>
+            <div className="app">
+                <Overview />
+            </div>
         </>
     );
 }
