bd4e34b5000426c3b4c601f4fe148b960ceb1ba8
Brandon <nardnob@users.noreply.github.com>
2023-08-30 23:19:31 -0500

    Move Projects out of Overview and into Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,6 @@
 import Overview from './Overview';
+import Card from 'react-bootstrap/Card';
+import Project from './Project';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 
@@ -7,6 +9,41 @@ export default function Home() {
         <>
             <div className="app">
                 <Overview />
+
+                <div className="mt-4"></div>
+
+                <Card>
+                    <Card.Body>
+                        <h2 class="overview-header">Personal Projects</h2>
+                        <Project 
+                            title="Kryptonite Mod"
+                            text="My personal minecraft mod. Development was in Java and Forge."
+                            imageSrc="images/kryptonite.png"
+                            link="https://github.com/nardnob/KryptoniteMod"
+                        />
+
+                        <Project 
+                            title="Line Charges"
+                            text="ASP.NET MVC Practice, required for a job interview."
+                            imageSrc="images/LineCharges.png"
+                            link="https://github.com/nardnob/LineCharges"
+                        />
+
+                        <Project 
+                            title="RS Lookup"
+                            text="Chrome extension that integrates with a third-party hiscores API."
+                            imageSrc="images/rslookup.png"
+                            link="https://github.com/nardnob/rslookup_chrome"
+                        />
+
+                        <Project 
+                            title="React Website"
+                            text="Personal website made in React. View the code for this website on Github."
+                            imageSrc="images/react_website.png"
+                            link="https://github.com/nardnob/reactjs"
+                        />
+                    </Card.Body>
+                </Card>
             </div>
         </>
     );
--- src/pages/Home/Overview.js
+++ src/pages/Home/Overview.js
@@ -3,7 +3,6 @@ import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Project from './Project';
 
 export default function Overview() {
     return (
@@ -32,41 +31,6 @@ export default function Overview() {
                             </div>
                         </Col>
                     </Row>
-                </Card.Body>
-            </Card>
-
-            <div className="mt-4"></div>
-
-            <Card>
-                <Card.Body>
-                    <h2 class="overview-header">Personal Projects</h2>
-                    <Project 
-                        title="Kryptonite Mod"
-                        text="My personal minecraft mod. Development was in Java and Forge."
-                        imageSrc="images/kryptonite.png"
-                        link="https://github.com/nardnob/KryptoniteMod"
-                    />
-
-                    <Project 
-                        title="Line Charges"
-                        text="ASP.NET MVC Practice, required for a job interview."
-                        imageSrc="images/LineCharges.png"
-                        link="https://github.com/nardnob/LineCharges"
-                    />
-
-                    <Project 
-                        title="RS Lookup"
-                        text="Chrome extension that integrates with a third-party hiscores API."
-                        imageSrc="images/rslookup.png"
-                        link="https://github.com/nardnob/rslookup_chrome"
-                    />
-
-                    <Project 
-                        title="React Website"
-                        text="Personal website made in React. View the code for this website on Github."
-                        imageSrc="images/react_website.png"
-                        link="https://github.com/nardnob/reactjs"
-                    />
                 </Card.Body>
             </Card>
         </>
