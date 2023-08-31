f6e1ada1607e9e8b9551aacc4e59510cfb08b7a3
Brandon <nardnob@users.noreply.github.com>
2023-08-31 00:53:48 -0500

    Adjust margin for overview-text

--- src/pages/Home/Overview.js
+++ src/pages/Home/Overview.js
@@ -12,12 +12,12 @@ export default function Overview() {
                     <Card>
                         <Card.Body>
                             <p className="overview-header">I’m a full-stack software developer in Fort Smith, AR</p>
-                            <p className="overview-text">
+                            <p className="overview-text-1">
                                 I specialize in .NET desktop and web development, but have experience with many
                                 technologies. I currently work as a contractor for a trucking company, ArcBest Technologies,
                                 where I've completed countless projects successfully, including numerous new applications from the ground up.
                             </p>
-                            <p className="overview-text">
+                            <p className="overview-text-2">
                                 I am on the job market for a fully-remote position.
                                 Please review my website and contact me to discuss more.
                             </p>
