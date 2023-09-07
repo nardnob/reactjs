8d7e5d48269c2b9748670b131f57fea953197ce9
Brandon <nardnob@users.noreply.github.com>
2023-09-07 12:20:51 -0500

    Reorder projects

--- src/pages/Home/Projects.js
+++ src/pages/Home/Projects.js
@@ -13,10 +13,10 @@ export default function Projects() {
                 <Row>
                     <Col xs={12} sm={6} md={4} lg={3}>
                         <Project
-                            title="Kryptonite Mod"
-                            text="My personal minecraft mod. Development was in Java and Forge."
-                            imageSrc="images/kryptonite.png"
-                            link="https://github.com/nardnob/KryptoniteMod"
+                            title="ASP.NET Core + Blazor"
+                            text="Practice website made in ASP.NET Core utilizing Blazor."
+                            imageSrc="images/aspnetcore.png"
+                            link="https://github.com/nardnob/aspnetcore"
                         />
                     </Col>
                     <Col xs={12} sm={6} md={4} lg={3}>
@@ -29,14 +29,6 @@ export default function Projects() {
                     </Col>
                     <Col xs={12} sm={6} md={4} lg={3}>
                         <Project
-                            title="RS Lookup"
-                            text="Chrome extension that integrates with a third-party hiscores API."
-                            imageSrc="images/rslookup.png"
-                            link="https://github.com/nardnob/rslookup_chrome"
-                        />
-                    </Col>
-                    <Col xs={12} sm={6} md={4} lg={3}>
-                        <Project
                             title="React Website"
                             text="Personal website made in React. View the code for this website on Github."
                             imageSrc="images/react_website.png"
@@ -45,10 +37,18 @@ export default function Projects() {
                     </Col>
                     <Col xs={12} sm={6} md={4} lg={3}>
                         <Project
-                            title="ASP.NET Core Practice"
-                            text="Practice website made in ASP.NET Core utilizing Blazor."
-                            imageSrc="images/aspnetcore.png"
-                            link="https://github.com/nardnob/aspnetcore"
+                            title="Kryptonite Mod"
+                            text="My personal minecraft mod. Development was in Java and Forge."
+                            imageSrc="images/kryptonite.png"
+                            link="https://github.com/nardnob/KryptoniteMod"
+                        />
+                    </Col>
+                    <Col xs={12} sm={6} md={4} lg={3}>
+                        <Project
+                            title="RS Lookup"
+                            text="Chrome extension that integrates with a third-party hiscores API."
+                            imageSrc="images/rslookup.png"
+                            link="https://github.com/nardnob/rslookup_chrome"
                         />
                     </Col>
                 </Row>
