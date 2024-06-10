e6d63151b74db5c05ac277cdf3947aa6fde52aa1
Brandon <nardnob@users.noreply.github.com>
2024-06-10 17:42:30 -0500

    Add Privacy Policy and footer (#10)
    * Add Privacy page and initial privacy policy.
    
    * Modify privacy policy to remove "signing in to Your Account" section
    
    * Remove irrelevant privacy policy information. Add styling.
    
    * Add a basic footer
    
    * Update footer with Privacy link. Remove Privacy Policy from header.
    
    * Adjust footer Links column
    
    * Adjust .app's min-height
    
    * Add m-top to footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Playground from './pages/Playground/Playground';
+import Privacy from './pages/Privacy/Privacy';
 import NavigationBar from './common/NavigationBar';
+import Footer from './common/Footer';
 import './App.css';
 
 export default function App() {
@@ -14,7 +16,9 @@ export default function App() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/playground" element={<Playground />} />
+                <Route path="/privacy" element={<Privacy />} />
             </Routes>
+            <Footer />
         </>
     )
 }
---
+++ src/common/Footer.js
@@ -0,0 +1,29 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="page-footer font-small blue pt-4">
+        <div className="container-fluid text-center text-md-left">
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <h4>Brandon Dixson - nardnob</h4>
+              <p>Feel free to contact me using the Contact form.</p>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0" />
+
+            <div className="col-md-6 mb-md-0 mb-3">
+              <h5>Important Links</h5>
+              <ul className="list-unstyled">
+                <li><a href="/privacy">Privacy Policy</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        
+        <div className="footer-copyright text-center py-3">© 2024 Copyright</div>
+      </footer>
+    </>
+  );
+}
