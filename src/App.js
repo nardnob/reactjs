d71cc02a767d940c0bf120f10834ab5f49c5fa85
Brandon <nardnob@users.noreply.github.com>
2024-06-10 17:10:50 -0500

    Add a basic footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About/About';
 import Playground from './pages/Playground/Playground';
 import Privacy from './pages/Privacy/Privacy';
 import NavigationBar from './common/NavigationBar';
+import Footer from './common/Footer';
 import './App.css';
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
                 <Route path="/playground" element={<Playground />} />
                 <Route path="/privacy" element={<Privacy />} />
             </Routes>
+            <Footer />
         </>
     )
 }
