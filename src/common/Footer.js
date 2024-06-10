f8753ad3d33926499238a6e327ee3fed5cc2dab4
Brandon <nardnob@users.noreply.github.com>
2024-06-10 17:29:56 -0500

    Update footer with Privacy link. Remove Privacy Policy from header.

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -7,12 +7,13 @@ export default function Footer() {
         <div className="container-fluid text-center text-md-left">
           <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
-              <h5 className="text-uppercase">Footer Content</h5>
-              <p>Here you can use rows and columns to organize your footer content.</p>
+              <h4>Brandon Dixson - nardnob</h4>
+              <p>Feel free to contact me using the Contact form.</p>
             </div>
 
             <hr className="clearfix w-100 d-md-none pb-0" />
 
+            {/*
             <div className="col-md-3 mb-md-0 mb-3">
               <h5 className="text-uppercase">Links</h5>
               <ul className="list-unstyled">
@@ -22,14 +23,12 @@ export default function Footer() {
                 <li><a href="#!">Link 4</a></li>
               </ul>
             </div>
+            */}
 
             <div className="col-md-3 mb-md-0 mb-3">
-              <h5 className="text-uppercase">Links</h5>
+              <h5>Important Links</h5>
               <ul className="list-unstyled">
-                <li><a href="#!">Link 1</a></li>
-                <li><a href="#!">Link 2</a></li>
-                <li><a href="#!">Link 3</a></li>
-                <li><a href="#!">Link 4</a></li>
+                <li><a href="/privacy">Privacy Policy</a></li>
               </ul>
             </div>
           </div>
--- src/common/NavigationBar.js
+++ src/common/NavigationBar.js
@@ -31,7 +31,6 @@ export default function NavigationBar() {
                             <Nav.Link href="home">Home</Nav.Link>
                             <Nav.Link href="about">About</Nav.Link>
                             <Nav.Link href="playground">Playground</Nav.Link>
-                            <Nav.Link href="privacy">Privacy Policy</Nav.Link>
                             <Nav.Link onClick={handleContactClick}>Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
